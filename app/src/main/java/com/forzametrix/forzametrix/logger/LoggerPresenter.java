f3ff5d0a3fbbc7f7a016d3d87d75e0e4c2e7510c
Bryan DenHollander <bryan.denhollander@gmail.com>
2017-11-07 22:07:12 -0500

    super simple VBT algorithm is functional. Next steps are updating the display to include the VBT information.

--- app/src/main/java/com/forzametrix/forzametrix/logger/LoggerPresenter.java
+++ app/src/main/java/com/forzametrix/forzametrix/logger/LoggerPresenter.java
@@ -33,10 +33,16 @@ import static android.support.v4.util.Preconditions.checkNotNull;
 
 public class LoggerPresenter implements LoggerContract.Presenter {
 
+    private float a,prevOutput;
     private float accelX,accelY,accelZ;
     private float gravX,gravY,gravZ;
     private String gravData, accelData,data;
     private long startTime;
+    private boolean logging;
+    private int rep;
+    private boolean accelReady;
+    private float cumVelocity,prevAccelMagnitude,cumAccelMagnitude;
+    int samples;
 
     private final DataRecorderContract.DataRecorder mDataRecorder;
 
@@ -47,6 +53,15 @@ public class LoggerPresenter implements LoggerContract.Presenter {
         mLoggerView = checkNotNull(loggerView,"loggerView cannot be null");
 
         mLoggerView.setPresenter(this);
+        prevOutput = 1;
+        a = (float)0.043;
+        logging = false;
+        accelReady = false;
+        rep = 0;
+        cumVelocity = 0.0f;
+        prevAccelMagnitude = 0.0f;
+        cumAccelMagnitude = 0.0f;
+        samples = 0;
     }
 
 
@@ -59,6 +74,7 @@ public class LoggerPresenter implements LoggerContract.Presenter {
             accelY = event.values[1];
             accelZ = event.values[2];
             accelData = event.values[0] + "," + event.values[1] + "," + event.values[2];
+            accelReady = true;
         }
         if(event.sensor.getType() == Sensor.TYPE_GRAVITY) {
             //update with the latest grav data;
@@ -66,20 +82,57 @@ public class LoggerPresenter implements LoggerContract.Presenter {
             gravY = event.values[1];
             gravZ = event.values[2];
             gravData = event.values[0] + "," + event.values[1] + "," + event.values[2];
-            //int forceInt = (int) (event.values[0] * 10);
-            //mLoggerView.updateForce(forceInt);
         }
+
+
+
 
         //calculate the dot product;
         float dp = accelX*gravX + accelY*gravY + accelZ*gravZ;
-        mLoggerView.updateForceView(Float.toString(dp));
-        mLoggerView.updateForce((int)dp-80);
+
+        //low pass filter the dot product
+        //y[k] = a * (x[k] - y[k-1]) + y[k-1]
+        //a = 1/T where T is the time constant
+
+        float output = a *((dp -  prevOutput) + prevOutput);
+
+        mLoggerView.updateForceView(String.format("%.3f",output));
+        mLoggerView.updateForce((int)(output*10));
 
         //data = data + currentTime + "," + dp + "\n";
+        //
 
+        if(output >= 5.00f && !logging){
+            logging = true;
+            prevAccelMagnitude = 0.0f;
+            cumVelocity = 0.0f;
+            cumAccelMagnitude = 0.0f;
+            samples = 0;
+            data = data + "-------------------------\n";
+        }
+
+        if(logging && output < 2.00f){
+            logging = false;
+            rep++;
+            float averageVelocity = cumVelocity/samples;
+            data = data + "------- " + rep + ":" + averageVelocity + " -------\n";
+        }
         //compile the string
         //data = data + currentTime + "," + accelData + "," + gravData + "\n";
 
+        if(logging && accelReady) {
+            //magnitude of the acceleration vector
+            double magnitude = Math.sqrt((accelX * accelX) + (accelY * accelY) + (accelZ * accelZ));
+
+            //rough velocity calculation. this assumes that samples are equally spaced in time, which is not necessarily true
+            cumAccelMagnitude += ((float)magnitude - prevAccelMagnitude)/2.0f;
+            cumVelocity += cumAccelMagnitude;
+            samples += 1;
+
+            data += samples + "," + currentTime + "," + String.format("%.3f", output) + "," + String.format("%.7f",magnitude) + "," + String.format("%.7f",cumVelocity) + "\n";
+            prevAccelMagnitude = (float)magnitude;
+            accelReady = false;
+        }
     }
 
     public final void onAccuracyChanged(Sensor sensor, int accuracy){
